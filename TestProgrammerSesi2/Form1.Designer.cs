e439dbb3cac7671d6481686e514d28b842980ee3
MuhammadArif686 <arief86.ma@gmail.com>
2018-10-10 07:46:32 -0700

    versi 1.0.o

--- TestProgrammerSesi2/Form1.Designer.cs
+++ TestProgrammerSesi2/Form1.Designer.cs
@@ -28,12 +28,230 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNim = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtAlamat = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnView = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtCariNama = new System.Windows.Forms.TextBox();
+            this.btnCariNama = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnCari = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NIM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Alamat";
+            // 
+            // txtNim
+            // 
+            this.txtNim.Location = new System.Drawing.Point(84, 13);
+            this.txtNim.Name = "txtNim";
+            this.txtNim.Size = new System.Drawing.Size(100, 20);
+            this.txtNim.TabIndex = 1;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(84, 43);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(258, 20);
+            this.txtNama.TabIndex = 2;
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Location = new System.Drawing.Point(84, 73);
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.Size = new System.Drawing.Size(627, 20);
+            this.txtAlamat.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnView);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 104);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(698, 182);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "[Data Mahasiswa]";
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(607, 18);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(75, 23);
+            this.btnView.TabIndex = 1;
+            this.btnView.Text = "View Data";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(677, 129);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtCariNama);
+            this.groupBox2.Controls.Add(this.btnCariNama);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(13, 293);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(698, 60);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "[Pencarian Data]";
+            // 
+            // txtCariNama
+            // 
+            this.txtCariNama.Location = new System.Drawing.Point(71, 21);
+            this.txtCariNama.Name = "txtCariNama";
+            this.txtCariNama.Size = new System.Drawing.Size(530, 20);
+            this.txtCariNama.TabIndex = 1;
+            // 
+            // btnCariNama
+            // 
+            this.btnCariNama.Location = new System.Drawing.Point(607, 19);
+            this.btnCariNama.Name = "btnCariNama";
+            this.btnCariNama.Size = new System.Drawing.Size(75, 23);
+            this.btnCariNama.TabIndex = 0;
+            this.btnCariNama.Text = "Cari";
+            this.btnCariNama.UseVisualStyleBackColor = true;
+            this.btnCariNama.Click += new System.EventHandler(this.btnCariNama_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Nama";
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(13, 376);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(75, 23);
+            this.btnSimpan.TabIndex = 6;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(107, 375);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 7;
+            this.btnEdit.Text = "Perbaiki";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(202, 374);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 23);
+            this.btnHapus.TabIndex = 8;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // btnCari
+            // 
+            this.btnCari.Location = new System.Drawing.Point(213, 9);
+            this.btnCari.Name = "btnCari";
+            this.btnCari.Size = new System.Drawing.Size(75, 23);
+            this.btnCari.TabIndex = 9;
+            this.btnCari.Text = "Cari";
+            this.btnCari.UseVisualStyleBackColor = true;
+            this.btnCari.Click += new System.EventHandler(this.btnCari_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(723, 422);
+            this.Controls.Add(this.btnCari);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtAlamat);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtNim);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Data Mahasiswa";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNim;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtAlamat;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCariNama;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnCari;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.TextBox txtCariNama;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- TestProgrammerSesi2/Form1.cs
+++ TestProgrammerSesi2/Form1.cs
@@ -7,14 +7,110 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace TestProgrammerSesi2
 {
     public partial class Form1 : Form
     {
+        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\Programming\Dekstop Aplication\TestProgrammerSesi2\TestProgrammerSesi2\Akademik.accdb");
+        int count = 0;
+        int hitung = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into mahasiswa values ('"+txtNim.Text+"', '"+txtNama.Text+"', '"+txtAlamat.Text+"')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            txtNim.Text = "";
+            txtNama.Text = "";
+            txtAlamat.Text = "";
+            MessageBox.Show("Data Berhasil Diinputkan");
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from mahasiswa";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from mahasiswa where nim = '"+txtNim.Text+"'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Data Berhasil Dihapus");
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update mahasiswa set nama = '"+txtNama.Text+"', alamat = '"+txtAlamat.Text+"' where nim = '"+txtNim.Text+"'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Data Berhasil DiUpdate");
+        }
+
+        private void btnCari_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from mahasiswa where nim = '"+txtNim.Text+"'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            da.Fill(dt);
+            count = Convert.ToInt32(dt.Rows.Count.ToString());
+            dataGridView1.DataSource = dt;
+            con.Close();
+
+            if (count == 0)
+            {
+                MessageBox.Show("Data Tidak Ditemukan");
+            }
+        }
+
+        private void btnCariNama_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            con.Open();
+            OleDbCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from mahasiswa where nama = '" + txtNama.Text + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            da.Fill(dt);
+            count = Convert.ToInt32(dt.Rows.Count.ToString());
+            dataGridView1.DataSource = dt;
+            con.Close();
+
+            if (count == 0)
+            {
+                MessageBox.Show("Data Tidak Ditemukan");
+            }
         }
     }
 }
